f3e71d07b0c094a950141ad78d8a6f77a4c5be5a
yolk00 <136626573+yolk00@users.noreply.github.com>
2023-09-25 09:50:06 -0400

    Added period section
    Basic design and functionality to period section.
    Also added barebones to foul and timeout sections (unsure if will implement).

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ let awayPts = 0;
 let awayPtsEl = document.getElementById("awayPoints");
 awayPtsEl.textContent = awayPts;
 
+let period = 0;
+let periodEl = document.getElementById("period");
+periodEl.textContent = period;
+
 // HOME SCORE
 function add3Home() {
   let homeScore = (homePts += 3);
@@ -64,4 +68,20 @@ function reset() {
   let awayScore = (awayPts *= 0);
   awayPtsEl.textContent = awayScore;
   console.log(awayScore);
+
+  period *= 0;
+  periodEl.textContent = period;
+}
+
+// PERIOD
+function nextPeriod() {
+  if (period < 4) {
+    period += 1;
+    periodEl.textContent = period;
+    console.log(period);
+  } else {
+    period = 4;
+    periodEl.textContent = period;
+    console.log(period);
+  }
 }
